56b0f43bbeab3a3432cce0e11b441687a1ca489f
Mikita Manko <djcomandos@gmail.com>
2014-11-23 12:26:29 -0800

    v++

--- server.js
+++ server.js
@@ -1,1 +1,3 @@
+// "server.js" file required at the root directory according to this doc
+// http://docs.aws.amazon.com/opsworks/latest/userguide/workinglayers-node.html
 require('./server/server.js').start();
