633d3e795d0f0e76c1a1094e72a0ca0240ef8f58
Mikita Manko <djcomandos@gmail.com>
2014-11-22 18:21:29 -0800

    v++

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ http.createServer(function (request, response) {
       response.writeHead(200, {
          'Content-Type': 'text/plain'
       });
-      response.write('BH on OpsWorks')
+      response.write('BH on OpsWorks - version 2.0')
       response.end();
 }).listen(80);
